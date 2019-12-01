eb92eee1cb5aa34a39b9bf3c4c7650f9d7a2a581
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-12-01 08:55:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,12 +6,14 @@
 
 
 class Dog
-  def initialize(dogs_name)
+  def initialize(dogs_name, dogs_breed = "Mutt")
     @name = dogs_name
+    @breed = dogs_breed
   end
   
-  def name=(dogs_name)
+  def name=(dogs_name, dogs_breed = "Mutt")
     @name = dogs_name
+    @breed = dogs_breed
   end
   
   def name
